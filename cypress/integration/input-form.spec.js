4c857ef25f183a71315e70f5d2a26ac1b0fe2458
Andrew Van Slaars <avanslaars@gmail.com>
2017-10-27 11:44:16 -0400

    Moved visits to beforeEach

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,14 +1,15 @@
 describe('Input form', () => {
-  it('focuses input on load', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3030')
+  })
 
+  it('focuses input on load', () => {
     cy.focused()
       .should('have.class', 'new-todo')
   })
 
-  it.only('accepts input', () => {
+  it('accepts input', () => {
     const typedText = 'Buy Milk'
-    cy.visit('http://localhost:3030')
 
     cy.get('.new-todo')
       .type(typedText)
