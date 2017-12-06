12eb1a65f6553f89977b03d441621864a2336c11
Andrew Van Slaars <avanslaars@gmail.com>
2017-12-06 16:09:04 -0500

    Revised smoke test part 2

--- cypress/integration/smoke-tests.spec.js
+++ cypress/integration/smoke-tests.spec.js
@@ -6,7 +6,7 @@ describe('Smoke tests', () => {
   })
 
   context('With no todos', () => {
-    it.only('Saves new todos', () => {
+    it('Saves new todos', () => {
       const items = [
         {text: 'Buy milk', expectedLength: 1},
         {text: 'Buy eggs', expectedLength: 2},
@@ -28,6 +28,65 @@ describe('Smoke tests', () => {
           cy.get('.todo-list li')
             .should('have.length', todo.expectedLength)
         })
+    })
+  })
+
+  context('With active todos', () => {
+    beforeEach(() => {
+      cy.fixture('todos')
+        .each(todo => {
+          const newTodo = Cypress._.merge(todo, {isComplete: false})
+          cy.request('POST', '/api/todos', newTodo)
+        })
+      cy.visit('/')
+    })
+
+    it('Loads existing data from the DB', () => {
+      cy.get('.todo-list li')
+        .should('have.length', 4)
+    })
+
+    it('Deletes todos', () => {
+      cy.server()
+      cy.route('DELETE', '/api/todos/*')
+        .as('delete')
+
+      cy.get('.todo-list li')
+        .each($el => {
+          cy.wrap($el)
+            .find('.destroy')
+            .invoke('show')
+            .click()
+
+          cy.wait('@delete')
+        })
+        .should('not.exist')
+    })
+
+  it('Toggles todos', () => {
+    const clickAndWait = ($el) => {
+      cy.wrap($el)
+        .as('item')
+        .find('.toggle')
+        .click()
+
+      cy.wait('@update')
+    }
+    cy.server()
+    cy.route('PUT', '/api/todos/*')
+      .as('update')
+
+    cy.get('.todo-list li')
+      .each($el => {
+        clickAndWait($el)
+        cy.get('@item')
+          .should('have.class', 'completed')
+      })
+      .each($el => {
+        clickAndWait($el)
+        cy.get('@item')
+          .should('not.have.class', 'completed')
+      })
     })
   })
 })
