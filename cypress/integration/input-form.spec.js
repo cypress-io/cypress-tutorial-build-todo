ceccfa5c68718c9298482b9c0e2027234418a84e
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-05 23:41:37 +0900

    focus

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,60 +1,58 @@
-describe('Input form', () => {
+describe("Input form", () => {
   beforeEach(() => {
-    cy.seedAndVisit([])
-  })
+    cy.seedAndVisit([]);
+  });
 
-  it('focuses input on load', () => {
-    cy.focused()
-      .should('have.class', 'new-todo')
-  })
+  it("focuses input on load 0", () => {
+    cy.visit("http://localhost:3030");
 
-  it('accepts input', () => {
-    const typedText = 'Buy Milk'
+    cy.focused().should("have.class", "new-todo");
+  });
 
-    cy.get('.new-todo')
-      .type(typedText)
-      .should('have.value', typedText)
-  })
+  it("focuses input on load", () => {
+    cy.focused().should("have.class", "new-todo");
+  });
 
-  context('Form submission', () => {
+  it("accepts input", () => {
+    const typedText = "Buy Milk";
+
+    cy.get(".new-todo").type(typedText).should("have.value", typedText);
+  });
+
+  context("Form submission", () => {
     beforeEach(() => {
-      cy.server()
-    })
+      cy.server();
+    });
 
-    it('Adds a new todo on submit', () => {
-      const itemText = 'Buy eggs'
-      cy.route('POST', '/api/todos', {
+    it("Adds a new todo on submit", () => {
+      const itemText = "Buy eggs";
+      cy.route("POST", "/api/todos", {
         name: itemText,
         id: 1,
-        isComplete: false
-      })
+        isComplete: false,
+      });
 
-      cy.get('.new-todo')
+      cy.get(".new-todo")
         .type(itemText)
-        .type('{enter}')
-        .should('have.value', '')
+        .type("{enter}")
+        .should("have.value", "");
 
-      cy.get('.todo-list li')
-        .should('have.length', 1)
-        .and('contain', itemText)
-    })
+      cy.get(".todo-list li").should("have.length", 1).and("contain", itemText);
+    });
 
-    it('Shows an error message on a failed submission', () => {
+    it("Shows an error message on a failed submission", () => {
       cy.route({
-        url: '/api/todos',
-        method: 'POST',
+        url: "/api/todos",
+        method: "POST",
         status: 500,
-        response: {}
-      })
+        response: {},
+      });
 
-      cy.get('.new-todo')
-        .type('test{enter}')
+      cy.get(".new-todo").type("test{enter}");
 
-      cy.get('.todo-list li')
-        .should('not.exist')
+      cy.get(".todo-list li").should("not.exist");
 
-      cy.get('.error')
-        .should('be.visible')
-    })
-  })
-})
+      cy.get(".error").should("be.visible");
+    });
+  });
+});
