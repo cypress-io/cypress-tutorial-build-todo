9b985f19c09825db2179660e3e0adcc37bc7abef
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-05 23:45:34 +0900

    this fails

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default props =>
+export default (props) => (
   <form onSubmit={props.handleTodoSubmit}>
     <input
-      type='text'
-      autoFocus
+      type="text"
       value={props.currentTodo}
       onChange={props.handleNewTodoChange}
       className="new-todo"
-      placeholder="What needs to be done?"/>
+      placeholder="What needs to be done?"
+    />
   </form>
+);
